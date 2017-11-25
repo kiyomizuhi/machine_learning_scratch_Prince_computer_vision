fd657a69c75f139a9fc95b587fa77e3920f1a6d7
Hiroyuki Inoue <hiroyukiinoue@Hiroyukis-MacBook-Pro.local>
2017-11-25 02:44:08 +0200

    revised

--- gaussian_mixture_discrete_1d.py
+++ gaussian_mixture_discrete_1d.py
@@ -1,99 +1,124 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  8 19:52:17 2017
-
-@author: hiroyukiinoue
-"""
 
 import numpy as np
-import scipy as sp
+import scipy.stats as spst
 import matplotlib.pyplot as plt
 
-nm = 3
-ns = 300
-
-mu = 15 * np.random.rand(nm)
-sg = 3 * (np.random.rand(nm) + 0.1)
-lm = (np.random.rand(nm) + 0.1)
-lm /= lm.sum()
-lm_ind = np.round(np.cumsum(lm) * ns).astype(int)
-lm_ind = np.insert(lm_ind, 0, 0)
-
-smp = np.zeros(ns)
-gs_true = np.zeros(ns)
-x = np.linspace(-20,20,400)
-
-clrs1 = [(0.6, 0, 0), (0, 0.6, 0), (0, 0, 0.6)]
-clrs2 = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
-plt.figure(1, figsize=(5,4))
-plt.clf()
-for k in range(nm):
-    this_smp = np.random.normal(mu[k], sg[k], lm_ind[k+1] - lm_ind[k])
-    smp[lm_ind[k]:lm_ind[k + 1]] = this_smp
-    gs_true[lm_ind[k]:lm_ind[k + 1]] = sp.stats.norm(mu[k], sg[k]).pdf(this_smp)
-    plt.hist(this_smp, 100, normed=1, color=clrs1[k])
-    plt.plot(x, sp.stats.norm(mu[k], sg[k]).pdf(x), color=clrs2[k])
-
-
-L_true = np.log(gs_true).sum() / ns # average log likelihood
-print('L_true', L_true)
-
-
-mue = 10 * np.random.rand(nm)
-sge = 3 * (np.random.rand(nm) + 0.1)
-lme = (np.random.rand(nm) + 0.1)
-lme /= lm.sum()
-
-plt.figure(2, figsize=(10,8))
-plt.clf()
-clr = 0.6
-clrs = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
-
-Nrep = 8
-for rep in range(Nrep):
-    gs = np.zeros((nm, ns))
-    for k in range(nm):
-        gs[k,:] = lme[k] * sp.stats.norm(mue[k], sge[k]).pdf(smp)
+def generate_synthetic_data(nm, ns):
+    '''synthesize a hypothetical data set'''
+    mu = 5 * np.random.rand(nm) + np.array([-7.5, 0, 7.5]) # mean
+    sg = 3 * (np.random.rand(nm) + 0.1)                # std
+    lm = (np.random.rand(nm) + 0.1)                    # mixture ratio (lambda)
+    lm /= lm.sum()
+    lm_ind = np.round(np.cumsum(lm) * ns).astype(int)  # tail index of each mix
+    lm_ind = np.insert(lm_ind, 0, 0)          
     
+    smp = np.zeros(ns)
+    gs_true = np.zeros(ns)
+    for k in range(nm):
+        this_smp = np.random.normal(mu[k], sg[k], lm_ind[k+1] - lm_ind[k])
+        smp[lm_ind[k]:lm_ind[k + 1]] = this_smp
+        gs_true[lm_ind[k]:lm_ind[k + 1]] = spst.norm(mu[k], sg[k]).pdf(this_smp)
+    
+    L_true = np.log(gs_true).sum() / ns # average log likelihood
+    return mu, sg, lm, lm_ind, smp, L_true
+
+def plot_synthetic_data(smp, mu, sg, lm, lm_ind, nm, ns):
+    '''plot the hypothetical data set'''
+    x = np.linspace(-20, 20, 400)
+    clrs1 = [(0.6, 0, 0), (0, 0.6, 0), (0, 0, 0.6)]
+    clrs2 = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
+    plt.hist(smp, 100, normed=1, color=(0.7, 0.7, 0.7))
+    for k in range(nm):
+        this_smp = smp[lm_ind[k]:lm_ind[k + 1]]
+        plt.plot(x, lm[k] * spst.norm(mu[k], sg[k]).pdf(x), color=clrs2[k])
+        plt.plot(this_smp, -0.05*np.ones(this_smp.shape), '.', color=clrs1[k], markersize=2) # 1D representation
+
+def generate_initial_state(nm, ns):
+    '''this will be the initial parameters for the EM algorithm'''
+    mue = 15 * np.random.rand(nm)  # mean
+    sge = 3 * (np.random.rand(nm) + 0.1)# std
+    lme = (np.random.rand(nm) + 0.1) # mixture ratio (lambda)
+    lme /= lme.sum()
+    return mue, sge, lme
+
+def e_step(smp, mue, sge, lme, nm, ns):
+    '''as the name says'''
+    gs = np.zeros((nm, ns)) # pdf of gaussians
+    for k in range(nm):
+        gs[k] = lme[k] * spst.norm(mue[k], sge[k]).pdf(smp)
     r = gs/gs.sum(axis=0)
-    ax = plt.subplot(Nrep/2, 6, 1 + 3 * rep + 0)
-    plt.hist(smp, 100, normed=1, color=(0.7, 0.7, 0.7))
-    for i in range(ns):
-        cl = (clr*r[0, i], clr*r[1, i], clr*r[2, i])
-        plt.plot(smp[i], -0.05, '.', color=cl, markersize=2)
-    for k, c in zip(range(nm), clrs):
-        plt.plot(x, lme[k] * sp.stats.norm(mue[k], sge[k]).pdf(x), color=clrs[k])
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        ax.set_ylim((-0.1, 0.3))
-        ax.set_title('E-step : %d' % (rep + 1))
-        
-    lme = r.sum(axis=1)/r.sum()
-    sge = np.sqrt((r*((np.tile(smp, (nm,1)) - mue[:,np.newaxis])**2)).sum(axis=1)/r.sum(axis=1))
-    mue = (r * smp).sum(axis=1)/r.sum(axis=1)
-    ax = plt.subplot(Nrep/2, 6, 1 + 3 * rep + 1)
-    plt.hist(smp, 100, normed=1, color=(0.7, 0.7, 0.7))
-    for i in range(ns):
-        cl = (clr*r[0, i], clr*r[1, i], clr*r[2, i])
-        plt.plot(smp[i], -0.05, '.', color=cl, markersize=2)
-    for k, c in zip(range(nm), clrs):
-        plt.plot(x, lme[k] * sp.stats.norm(mue[k], sge[k]).pdf(x), color=clrs[k])
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        ax.set_ylim((-0.1, 0.3))
-        ax.set_title('M-step : %d' % (rep + 1))
-    
-    ax = plt.subplot(Nrep/2, 6, 1 + 3 * rep + 2)
-    plt.hist(smp, 100, normed=1, color=(0.7, 0.7, 0.7))    
-    for k in range(nm):
-        plt.plot(smp[lm_ind[k]:lm_ind[k + 1]], -0.05*np.ones(lm_ind[k + 1] - lm_ind[k]), '.', color=clrs[k], markersize=2)
-        plt.plot(x, lm[k] * sp.stats.norm(mu[k], sg[k]).pdf(x), color=clrs2[k])
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        ax.set_ylim((-0.1, 0.3))
-        ax.set_title('truth : %d' % (rep + 1))
-    
-    # update the average log likelihood
     L_infer = np.log(gs.sum(axis=0)).sum() / ns # average log likelihood
-    print('L_infer = %2.6f , L_true = %2.6f' % (L_infer, L_true))
+    return r, L_infer
+    
+def m_step(smp, r, nm, ns):
+    '''as the name says'''
+    lme = r.sum(axis=1)/r.sum()
+    mue = (r * smp).sum(axis=1)/r.sum(axis=1)
+    sge = np.sqrt((r*((np.tile(smp, (nm,1)) - mue[:,np.newaxis])**2)).sum(axis=1)/r.sum(axis=1))
+    return mue, sge, lme
+
+def plot_em_steps(smp, r, mue, sge, lme, nm, ns):
+    '''plot the each step of EM algorithm'''
+    x = np.linspace(-20, 20, 400)
+    clr = 0.6
+    clrs = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
+    plt.hist(smp, 100, normed=1, color=(0.7, 0.7, 0.7))
+    for i in range(ns):
+        cl = (clr*r[0, i], clr*r[1, i], clr*r[2, i])
+        plt.plot(smp[i], -0.05, '.', color=cl, markersize=2)
+    for k, c in zip(range(nm), clrs):
+        plt.plot(x, lme[k] * spst.norm(mue[k], sge[k]).pdf(x), color=clrs[k])
+
+def main(nm, ns, Nrep):
+    '''usually the repetition of EM steps continues till the log likelihood 
+       saturates. For the sake of plotting every step, I use a for loop 
+       instead of a while loop.'''
+    
+    mu, sg, lm, lm_ind, smp, L_true = generate_synthetic_data(nm, ns)
+    plt.figure(1, figsize=(5,4))
+    plt.clf()
+    plot_synthetic_data(smp, mu, sg, lm, lm_ind, nm, ns)
+    
+    mue, sge, lme = generate_initial_state(nm, ns)
+    axi = 0 # subplot number
+    plt.figure(2, figsize=(12,9))
+    plt.clf()
+    for rep in range(Nrep):
+        # E-step
+        r, L_infer = e_step(smp, mue, sge, lme, nm, ns)
+        axi += 1 
+        ax = plt.subplot(Nrep/2, 6, axi)
+        plot_em_steps(smp, r, mue, sge, lme, nm, ns)
+        ax.set_title('E-step : %d' % (rep + 1))
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_ylim((-0.1, 0.3))
+
+        # M-step
+        mue, sge, lme = m_step(smp, r, nm, ns)
+        axi += 1 
+        ax = plt.subplot(Nrep/2, 6, axi)
+        plot_em_steps(smp, r, mue, sge, lme, nm, ns)
+        ax.set_title('M-step : %d' % (rep + 1))
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_ylim((-0.1, 0.3))
+
+        # plot the ground truth for comparison
+        axi += 1 
+        ax = plt.subplot(Nrep/2, 6, axi)
+        plot_synthetic_data(smp, mu, sg, lm, lm_ind, nm, ns)
+        ax.set_title('grn_truth')
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_ylim((-0.1, 0.3))
+
+        print('L_infer = %2.6f , L_true = %2.6f' % (L_infer, L_true))
+
+if __name__ == '__main__':
+    Nrep = 8
+    num_mixedgs = 3 
+    num_samples = 300
+    main(num_mixedgs, num_samples, Nrep)
--- gaussian_mixture_discrete_2d.py
+++ gaussian_mixture_discrete_2d.py
@@ -14,7 +14,7 @@ def generate_nice_covariance_for_ellipse(k):
     ''' this function outputs an covariance matrices avoiding generating too much squashed ellipses'''
     sg = np.zeros((k, 2, 2))
     for i in range(k):
-        eig = 2*np.random.rand(2) + 0.5 # adding 0.2 and normalize to avoid generating smashed ellipses
+        eig = 2 * np.random.rand(2) + 0.5 # adding 0.2 and normalize to avoid generating smashed ellipses
         ang = np.pi * np.random.rand(1) # in radian
         c, s = np.cos(ang), np.sin(ang)
         rot = np.matrix(np.asarray([[c, s], [-s, c]]))
@@ -32,97 +32,120 @@ def contour_ellipse(mu, sg):
     elp = mu[:,np.newaxis] + elp_rot # translate the ellipse
     return elp
 
-dim = 2    # dimension of the problem. Suppose we work on a 2D problem.
-nm = 3     # number of mixed gaussian. Here, we set it to 3.
-ns = 300   # number of samples
+def generate_synthetic_data(nm, ns, dim):
+    '''synthesize a hypothetical data set'''
+    mu = 3.0 * np.random.rand(nm, dim) +\
+         7.5*np.array([[1., 0.],[0., 1.],[1., 1.]]) # mean
+    sg = generate_nice_covariance_for_ellipse(nm)   # this is to ensure the positive-definiteness of the sigma
+    lm = np.random.rand(nm) + 0.1                   # lambda : ratio of the mixture. 0.1 is to make sure it won't generate too tiny weight
+    lm /= lm.sum()
+    lm_ind = np.round(np.cumsum(lm) * ns).astype(int)
+    lm_ind = np.insert(lm_ind, 0, 0)
 
-mu = 10 * np.random.rand(nm, dim)             # mean
-sg = generate_nice_covariance_for_ellipse(nm)  # this is to ensure the positive-definiteness of the sigma
-lm = np.random.rand(nm) + 0.1           # lambda : ratio of the mixture. 0.1 is to make sure it won't generate too tiny weight
-lm /= lm.sum()
-lm_ind = np.round(np.cumsum(lm) * ns).astype(int)
-lm_ind = np.insert(lm_ind, 0, 0)
+    smp = np.zeros((dim, ns))
+    gs_true = np.zeros(ns)
+    for k in range(nm):
+        this_smp = np.random.multivariate_normal(mu[k], sg[k], lm_ind[k+1] - lm_ind[k]).T
+        smp[:, lm_ind[k]:lm_ind[k + 1]] = this_smp
+        gs_true[lm_ind[k]:lm_ind[k + 1]] = multivariate_normal(mu[k], sg[k]).pdf(this_smp.T)
+        
+    L_true = np.log(gs_true).sum() / ns # average log likelihood
+    return mu, sg, lm, lm_ind, smp, L_true
 
-smp = np.zeros((dim, ns))
-gs_true = np.zeros(ns)
-clrs1 = [(0.6, 0, 0), (0, 0.6, 0), (0, 0, 0.6)]
-clrs2 = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
-plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
-plt.figure(1)
-plt.clf()
-for k in range(nm):
-    this_smp = np.random.multivariate_normal(mu[k], sg[k], lm_ind[k+1] - lm_ind[k]).T
-    smp[:, lm_ind[k]:lm_ind[k + 1]] = this_smp
-    gs_true[lm_ind[k]:lm_ind[k + 1]] = multivariate_normal(mu[k], sg[k]).pdf(this_smp.T)
-    plt.plot(this_smp[0], this_smp[1], '.', markersize=3, color=clrs1[k])
-    elp = contour_ellipse(mu[k], sg[k])
-    plt.plot(elp[0], elp[1], color=clrs2[k], ls='-')
+def plot_synthetic_data(smp, mu, sg, lm, lm_ind, nm, ns):
+    '''plot the hypothetical data set'''
+    clrs1 = [(0.6, 0, 0), (0, 0.6, 0), (0, 0, 0.6)]
+    clrs2 = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
+    for k in range(nm):
+       this_smp = smp[:, lm_ind[k]:lm_ind[k + 1]]
+       plt.plot(this_smp[0], this_smp[1], '.', markersize=3, color=clrs1[k])
+       elp = contour_ellipse(mu[k], sg[k])
+       plt.plot(elp[0], elp[1], color=clrs2[k], ls='-')
 
-L_true = np.log(gs_true).sum() / ns # average log likelihood
-print('L_true', L_true)
+def generate_initial_state(nm, ns, dim):
+    '''this will be the initial parameters for the EM algorithm'''
+    mue = 10 * np.random.rand(nm, dim)             # initial mean (to be Estimated)
+    sge = generate_nice_covariance_for_ellipse(nm) # initial std  (to be Estimated)
+    lme = np.random.rand(nm)
+    lme /= lme.sum()                      # initial mixture ratio (to be Estimated)
+    return mue, sge, lme
 
-mue = 10 * np.random.rand(nm, dim)             # initial mean (to be Estimated)
-sge = generate_nice_covariance_for_ellipse(nm) # initial std  (to be Estimated)
-lme = np.random.rand(nm)
-lme /= lme.sum()                      # initial mixture ratio (to be Estimated)
-
-Nrep = 16
-plt.rcParams['figure.figsize'] = (16.0, 20.0) # set default size of plots
-clr = 0.6
-clrs = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
-
-plt.figure(2)
-plt.clf()
-# usually the repetition is terminated once the log likelihood saturates.
-# For the sake of plotting every step, I use a for loop instead of a while loop.
-for rep in range(Nrep):
-    
-    # E-step
+def e_step(smp, mue, sge, lme, nm, ns):
+    '''as the name says'''
     gs = np.zeros((nm, ns))
     for k in range(nm):
         gs[k, :] = lme[k] * multivariate_normal(mue[k], sge[k]).pdf(smp.T)
     r = gs / gs.sum(axis=0) # sum over classes
-    # plot the update
-    ax = plt.subplot(Nrep/2, 6, 1 + 3 * rep)
-    for i in range(ns):
-        cl = (clr*r[0, i], clr*r[1, i], clr*r[2, i])
-        plt.plot(smp[0, i], smp[1, i], '.', color=cl, markersize=2)
-    for i, c in zip(range(nm), clrs):
-        elp = contour_ellipse(mue[i], sge[i])
-        plt.plot(elp[0], elp[1], color=c, ls='-')
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        ax.set_title('E-step : %d' % (rep + 1))
-        
-    # M-step
+    L_infer = np.log(gs.sum(axis=0)).sum() / ns # average log likelihood
+    return r, L_infer
+    
+def m_step(smp, mue, sge, lme, r, nm, ns):
+    '''as the name says'''
     lme = r.sum(axis=1)/r.sum()
     for k in range(nm):
+        mue[k] = (r[k] * smp).sum(axis=1) / r[k].sum()
         dlts = smp - mue[k][:, np.newaxis]
-        sge[i] = np.dot((r[k] * dlts), dlts.T) / r[k].sum()
-        mue[i] = (r[k] * smp).sum(axis=1) / r[k].sum()
-    # plot the update
-    ax = plt.subplot(Nrep/2, 6, 1 + 3 * rep + 1)
+        sge[k] = np.dot((r[k] * dlts), dlts.T) / r[k].sum()
+    return mue, sge, lme
+
+def plot_em_steps(smp, r, mue, sge, lme, nm, ns):
+    '''plot the each step of EM algorithm'''
+    clr = 0.6
+    clrs = [(1, 0.2, 0), (0, 1, 0), (0, 0.6, 1)]
     for i in range(ns):
         cl = (clr*r[0, i], clr*r[1, i], clr*r[2, i])
         plt.plot(smp[0, i], smp[1, i], '.', color=cl, markersize=2)
     for i, c in zip(range(nm), clrs):
         elp = contour_ellipse(mue[i], sge[i])
         plt.plot(elp[0], elp[1], color=c, ls='-')
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        ax.set_title('M-step : %d' % (rep + 1))
-        
-    # plot the ground truth for a comparison
-    ax = plt.subplot(Nrep/2, 6, 1 + 3 * rep + 2)
-    for i, c in zip(range(nm), clrs):
-        this_smp = smp[:, lm_ind[i]:lm_ind[i+1]]
-        plt.plot(this_smp[0], this_smp[1], '.', markersize=2, color=clrs1[i])
-        elp = contour_ellipse(mu[i], sg[i])
-        plt.plot(elp[0], elp[1], color=clrs2[i], ls='-')
-        ax.set_title('ground truth')
+
+def main(nm, ns, dim, Nrep):
+    '''usually the repetition of EM steps continues till the log likelihood 
+       saturates. For the sake of plotting every step, I use a for loop 
+       instead of a while loop.'''
+    
+    mu, sg, lm, lm_ind, smp, L_true = generate_synthetic_data(nm, ns, dim)
+    plt.figure(1, figsize=(5,4))
+    plt.clf()
+    plot_synthetic_data(smp, mu, sg, lm, lm_ind, nm, ns)
+    
+    mue, sge, lme = generate_initial_state(nm, ns, dim)
+    axi = 0 # subplot number
+    plt.figure(2, figsize=(12,9))
+    plt.clf()
+    for rep in range(Nrep):
+        # E-step
+        r, L_infer = e_step(smp, mue, sge, lme, nm, ns)
+        axi += 1 
+        ax = plt.subplot(Nrep/2, 6, axi)
+        plot_em_steps(smp, r, mue, sge, lme, nm, ns)
+        ax.set_title('E-step : %d' % (rep + 1))
         ax.set_yticklabels([])
         ax.set_xticklabels([])
 
-    # update the average log likelihood
-    L_infer = np.log(gs.sum(axis=0)).sum() / ns # average log likelihood
-    print('L_infer = %2.6f , L_true = %2.6f' % (L_infer, L_true))
+        # M-step
+        mue, sge, lme = m_step(smp, mue, sge, lme, r, nm, ns)
+        axi += 1 
+        ax = plt.subplot(Nrep/2, 6, axi)
+        plot_em_steps(smp, r, mue, sge, lme, nm, ns)
+        ax.set_title('M-step : %d' % (rep + 1))
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+
+        # plot the ground truth for comparison
+        axi += 1 
+        ax = plt.subplot(Nrep/2, 6, axi)
+        plot_synthetic_data(smp, mu, sg, lm, lm_ind, nm, ns)
+        ax.set_title('grn_truth')
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+
+        print('L_infer = %2.6f , L_true = %2.6f' % (L_infer, L_true))
+
+    
+if __name__ == '__main__':
+    dim = 2  # dimension of the problem. Suppose we work on a 2D problem.
+    Nrep = 16 # number of repetition of EM steps
+    num_mixedgs = 3    # number of mixed gaussian. Here, we set it to 3.
+    num_samples = 300  # number of samples
+    main(num_mixedgs, num_samples, dim, Nrep)
